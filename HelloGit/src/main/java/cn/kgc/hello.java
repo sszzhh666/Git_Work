c9c749fca71ddd3032cc29bf78a1f29a7cd110c7
24007 <sszzhh666666@163.com>
2020-08-11 19:10:33 +0800

    本地hello1

---
+++ HelloGit/src/main/java/cn/kgc/hello.java
@@ -0,0 +1,12 @@
+package cn.kgc;/*
+*   @author  szhstart 
+*   @create  2020/8/11 9:51
+*
+*
+*/
+
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello1");
+    }
+}
